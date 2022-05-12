b9c5ec0833efc14ddf8b56f09f38d490b00ca2f6
Curtis Schembri <curtis.schembri@transport.nsw.gov.au>
2022-05-12 13:41:27 +1000

    Remove unneeded params around arg passing and apply discard operator where arg need not be passed

--- ProgramExit/ExitMessages.cs
+++ ProgramExit/ExitMessages.cs
@@ -4,22 +4,22 @@ internal static class ExitMessages
 {
     internal static string GetResultMsg(ExitCode code, string arg = EmptyString)
     {
-        return Messages[code](arg) + "... Exiting";
+        return $"{Messages[code](arg)}, exiting...";
     }
 
     private static readonly Dictionary<ExitCode, Func<string, string>> Messages = new()
     {
 #if RELEASE
-        { ExitCode.TimesheetCommitted, (arg) => $"Timothy committed your timesheet in {arg} milliseconds" },
+        { ExitCode.TimesheetCommitted, arg => $"Timothy committed your timesheet in {arg} milliseconds" },
 #else
-        { ExitCode.TimesheetCommitted, (arg) => $"Timothy would have committed your timesheet in {arg} milliseconds (debug build)" },
+        { ExitCode.TimesheetCommitted, arg => $"Timothy would have committed your timesheet in {arg} milliseconds (debug build)" },
 #endif // RELEASE
-        { ExitCode.InvalidArgumentCount, (arg) => "Timothy requires two arguments, command and target user (e.g., 'commit' and 'user@email.com')" },
-        { ExitCode.InvalidArgumentSpecified, (arg) => $"'{arg}' is an unrecognised command" },
-        { ExitCode.LoginDetailsIncorrect, (arg) => "Entered username or password is incorrect" },
-        { ExitCode.JobsFileNotFound, (arg) => "Could not find 'jobs.json' file" },
-        { ExitCode.DayMissingEntries, (arg) => $"Day '{arg}' was defined, but no entries were defined for this day" },
-        { ExitCode.EntryMissingJobCode, (arg) => $"Day '{arg}' has an entry missing a job code" },
-        { ExitCode.EntryMissingHours, (arg) => $"Day '{arg}' has an entry missing hours" }
+        { ExitCode.InvalidArgumentCount,     _ => "Timothy requires two arguments, command and target user (e.g., 'commit' and 'user@email.com')" },
+        { ExitCode.InvalidArgumentSpecified, arg => $"'{arg}' is an unrecognised command" },
+        { ExitCode.LoginDetailsIncorrect,    _ => "Entered username or password is incorrect" },
+        { ExitCode.JobsFileNotFound,         _ => "Could not find 'jobs.json' file" },
+        { ExitCode.DayMissingEntries,        arg => $"Day '{arg}' was defined, but no entries were defined for this day" },
+        { ExitCode.EntryMissingJobCode,      arg => $"Day '{arg}' has an entry missing a job code" },
+        { ExitCode.EntryMissingHours,        arg => $"Day '{arg}' has an entry missing hours" }
     };
 }
